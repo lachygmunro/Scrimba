14519ad21b415bcb9e0e8416ff778c7201362339
Lachlan Munro <lachlangmunro@gmail.com>
2022-04-19 23:51:20 +1000

    Completed passenger counter project.

--- JavaScript/PassengerCounter/index.js
+++ JavaScript/PassengerCounter/index.js
@@ -1,12 +1,15 @@
-
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0;
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
-  console.log(count)
+  let counter = count + " - "
+  saveEl.textContent += counter
+  count = 0
+  countEl.textContent = count;
 }
